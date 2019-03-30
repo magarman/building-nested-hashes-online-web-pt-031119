7879bad4e74490e692d15a611f62e2aaa32f140e
Mari Garman <garman.mariana@gmail.com>
2019-03-30 13:20:31 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,12 +1,14 @@
-def first_challenge
-   epic_tragedy = {montague: 
-   {patriarch: {}, 
-   matriarch: {}, 
-   hero: {}, 
-   hero_friends: []}, 
-   capulet: 
-   {patriarch: {}, 
-   matriarch: {}, 
-   heroine: {}, 
-   heroine_friends: []}}
-end
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
